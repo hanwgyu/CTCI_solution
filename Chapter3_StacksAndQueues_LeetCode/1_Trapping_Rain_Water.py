b36f0824095d1544a56bc2bdc5d91a0c49f65f36
hanwgyu <han.wgyu@gmail.com>
2019-07-23 21:27:31 +0900

    Revise comment

--- Chapter3_StacksAndQueues_LeetCode/1_Trapping_Rain_Water.py
+++ Chapter3_StacksAndQueues_LeetCode/1_Trapping_Rain_Water.py
@@ -1,7 +1,9 @@
 #Time complexity : O(N), Space complexity : O(1)
 class Solution(object):
     def trap(self, height):
-        #Solution : 가장 높은 Wall을 찾음. 양끝에서 시작해서, 가장 높은 wall에 다다를때까지 올라가거나 같은 높이의 WALL을 마주칠때마다 값을 즉시 계산.
+        # Solution : 가장 높은 Wall을 찾음. 양끝에서 시작해서, 가장 높은 wall에 다다를때까지 계산.
+        # 지금까지 마주친 가장 높은 블록을 Wall로 설정하여, 새로운 블록을 마주칠때마다 차이값을 계산하여 더함.
+        # 현재 Wall과 같은 높이거나 더 높은 블록을 마주칠때마다 Wall을 업데이트.
         
         if not height:
             return 0
