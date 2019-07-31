00c1f18cbb3f65db301fb2efb326ce553a4738e9
hanwgyu <han.wgyu@gmail.com>
2019-07-31 20:56:58 +0900

    Update K_Cloest_Points_to_Origin.py

--- Leetcode/K_Cloest_Points_to_Origin.py
+++ Leetcode/K_Cloest_Points_to_Origin.py
@@ -1,15 +1,17 @@
 class Solution(object):
+    #Time Complexity : O(NlogK), Space Complexity : O(K)
+    def kClosest(self, points, K):
+        a = []
+        for point in points:
+            heapq.heappush(a, (-(point[0]**2 + point[1]**2), point))
+            if len(a) == K+1:
+                heapq.heappop(a)
+        return [[i[1][0], i[1][1]] for i in a] 
+             
+    
     #Time Complexity : O(NlogN), Space Complexity : O(N)
     def kClosest(self, points, K):
-        d = []
-        for i in range(len(points)):
-            d.append((points[i][0]**2 + points[i][1]**2, i)) # (distance, index)
-        
-        d.sort(key = lambda e : e[0])
-        
-        ans = []
-        for i in range(K):
-            ans.append(points[d[i][1]])
-        return ans
+        sorted_points = sorted(points, key = lambda point: (point[0]**2 + point[1]**2))
+        return sorted_points[:K]
         
         
