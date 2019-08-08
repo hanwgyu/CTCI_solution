afac5a47ed6d522e0d970d7654a20c15861c71ab
hanwgyu <han.wgyu@gmail.com>
2019-08-09 00:30:58 +0900

    Create Copy_List_with_Random_Pointer.py

---
+++ Leetcode/Copy_List_with_Random_Pointer.py
@@ -0,0 +1,26 @@
+# Solution : 1) Copy node from head to tail. Set 'random' of new copy node as 'random' of origin node. Then set 'random' of origin node as new copy node.   
+# 2) Change 'random' of copy node as 'random' of origin node in same index
+# Time : O(N), Space : O(N)
+
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, next, random):
+        self.val = val
+        self.next = next
+        self.random = random
+"""
+class Solution(object):
+    def copyRandomList(self, head):
+        copy = dummy = Node(0, None, None)
+        origin = head
+        while origin:
+            copy.next = Node(origin.val, origin.next, origin.random)
+            origin.random = copy.next
+            copy, origin = copy.next, origin.next
+        copy = dummy
+        while copy:
+            if copy.random:
+                copy.random = copy.random.random        
+            copy = copy.next
+        return dummy.next
