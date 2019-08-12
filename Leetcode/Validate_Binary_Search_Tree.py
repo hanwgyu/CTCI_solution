826b379d89c6e5bb8eb32664736e56c2fc8b9d49
hanwgyu <han.wgyu@gmail.com>
2019-08-13 00:13:18 +0900

    Create Validate_Binary_Search_Tree.py

---
+++ Leetcode/Validate_Binary_Search_Tree.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# Solution : Check with in-order traversal. If current value becomes smaller than previous value, then return False.
+# Time : O(N), Space : O(1)
+# Python3
+
+class Solution(object):
+    def isValidBST(self, root):
+        def inOrder(node):
+            if not node:
+                return True
+            if not inOrder(node.left):
+                return False
+            nonlocal val
+            if val >= node.val:
+                return False
+            val = node.val
+            if not inOrder(node.right):
+                return False
+            return True
+        val = float('-inf')
+        return inOrder(root)
+        
