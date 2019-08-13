56c6f3f52c093488e8efbaf91089bdc617fd13a1
hanwgyu <han.wgyu@gmail.com>
2019-08-13 22:37:33 +0900

    Update Symmetric_Tree.py

--- Leetcode/Symmetric_Tree.py
+++ Leetcode/Symmetric_Tree.py
@@ -9,7 +9,15 @@
 #Time : O(N), Space : O(N)
 
 class Solution:
-    def isSymmetric(self, root: TreeNode) -> bool:
+    def isSymmetric_1(self, root: TreeNode) -> bool:
+        def traverse(node1: TreeNode, node2: TreeNode) -> bool:
+            if not node1 and not node2:
+                return True
+            if not node1 or not node2:
+                return False
+            return (node1.val == node2.val) and traverse(node1.left, node2.right) and traverse(node1.right, node2.left)   
+    
+    def isSymmetric_2(self, root: TreeNode) -> bool:
         def preOrder(node: TreeNode, ltor : bool):
             if not node:
                 if ltor:
