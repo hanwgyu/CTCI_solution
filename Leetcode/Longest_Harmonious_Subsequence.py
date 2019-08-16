f74257a0c466efef0a3a3894cc40e625ee271eb8
hanwgyu <han.wgyu@gmail.com>
2019-08-16 17:10:51 +0900

    Create Longest_Harmonious_Subsequence.py

---
+++ Leetcode/Longest_Harmonious_Subsequence.py
@@ -0,0 +1,16 @@
+# Time : O(N), Space : O(N)
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+        d = dict()
+        for num in nums:
+            if num in d:
+                d[num] += 1
+            else:
+                d[num] = 1
+        ans = 0
+        for num in nums:
+            if (num+1) in d:
+                l = d[num] + d[num+1]
+                ans = max(ans, l)
+        return ans
+            
