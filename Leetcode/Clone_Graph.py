5c7ad654b7451f3c562990e978330779f91bfc52
hanwgyu <han.wgyu@gmail.com>
2019-08-19 22:35:36 +0900

    Create Clone_Graph.py

---
+++ Leetcode/Clone_Graph.py
@@ -0,0 +1,24 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, neighbors):
+        self.val = val
+        self.neighbors = neighbors
+"""
+
+#Solution : DFS. Time : O(N), Space : O(N)
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        def dfs(node: 'Node') -> 'Node':
+            if node in visited:
+                return visited[node]
+            clone_node = Node(node.val, [])
+            visited[node] = clone_node
+            for neighbor in node.neighbors:
+                clone_node.neighbors.append(dfs(neighbor))
+            return clone_node
+        visited = dict()
+        return dfs(node)
+            
+        
