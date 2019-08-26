5aabc7ba1121b040945225cd7004fd6ba43be02d
hanwgyu <han.wgyu@gmail.com>
2019-08-26 11:58:01 +0900

    Create Subsets.py

---
+++ Leetcode/Subsets.py
@@ -0,0 +1,13 @@
+# Time : O(N*2^N), Space: O(2^N)
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        for i in range(2**len(nums)):
+            n, a = 0, []
+            while i > 0:
+                if i % 2 == 1:
+                    a.append(nums[n])
+                i, n = i >> 1, n+1
+            ans.append(a)
+        return ans
