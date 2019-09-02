0e6ace9411bad8508c9e239067527dcdf934e06f
hanwgyu <han.wgyu@gmail.com>
2019-09-02 21:34:41 +0900

    Create Happy_Number.py

---
+++ Leetcode/Happy_Number.py
@@ -0,0 +1,15 @@
+# Solution 1 : Use Hash
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        nums = set()
+        while n > 1:
+            temp = 0
+            while n > 0:
+                temp, n = temp + (n%10)**2, n//10
+            n = temp
+            if temp not in nums:
+                nums.add(temp)
+            else:
+                return False
+        return True
