7ef8a5dd8a6571bc8ba99b27ad19a385fee2a210
hanwgyu <han.wgyu@gmail.com>
2019-09-04 23:18:54 +0900

    Create Perfect_Squares.cc

---
+++ Leetcode/Perfect_Squares.cc
@@ -0,0 +1,27 @@
+// Solution 1 : DP. 
+// Time : O(N^2), Space : O(N)
+
+class Solution {
+public:
+    int numSquares(int n) {
+        static std::vector<int> min_nums = {0};
+        while(min_nums.size() <= n) {
+            int min_num = INT_MAX, i = min_nums.size();
+            for(int j = 1; j*j <= i; j++)
+                min_num = min(1 + min_nums[i-j*j], min_num);
+            min_nums.push_back(min_num);
+        }
+        return min_nums[n];
+    }
+    
+    int numSquares_1(int n) {
+        std::vector<int> min_nums = {0};
+        for(int i = 1; i < n+1; i++) {
+            int min_num = INT_MAX;
+            for(int j = 1; j*j <= i; j++)
+                min_num = min(1 + min_nums[i-j*j], min_num);
+            min_nums.push_back(min_num);
+        }
+        return min_nums[n];
+    }
+};
