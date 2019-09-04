e041fb09478d29d789ffc28a86b56584ea488ad6
hanwgyu <han.wgyu@gmail.com>
2019-09-04 21:19:44 +0900

    Update Minimum_Height_Trees.py

--- Leetcode/Minimum_Height_Trees.py
+++ Leetcode/Minimum_Height_Trees.py
@@ -8,9 +8,9 @@ class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
         def dfs(n : int, visited : Set[int]) -> List[int]:
             if n in visited:
-                return list()
+                return []
             visited.add(n)
-            p_1, p_2 = list(), list()
+            p_1, p_2 = [], []
             for adj in graph[n]:
                 p = dfs(adj, visited)
                 if len(p) > len(p_1):
@@ -24,7 +24,7 @@ class Solution:
                 self.max_p = p_1 + [n] + p_2[::-1]
             p_1.append(n)
             return p_1
-        
+   
         if not edges:
             return [0]
         self.max_h, self.max_p, graph = 0, None, defaultdict(list)
@@ -32,4 +32,4 @@ class Solution:
             graph[src].append(dst)
             graph[dst].append(src)
         dfs(edges[0][0], set())
-        return [self.max_p[self.max_h//2]] if self.max_h%2 == 1 else [self.max_p[(self.max_h-1)//2], self.max_p[self.max_h//2]]
+        return self.max_p[(self.max_h-1)//2 : self.max_h//2+1]
