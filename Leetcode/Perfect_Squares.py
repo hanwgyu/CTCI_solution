20ac5cfb6a9b6d136ec6d29eb13129b9f53b425b
hanwgyu <han.wgyu@gmail.com>
2019-09-04 23:41:13 +0900

    Create Perfect_Squares.py

---
+++ Leetcode/Perfect_Squares.py
@@ -0,0 +1,31 @@
+# Solution 1 : BFS. 
+# Time : O(N^2), Space : O(N)
+
+# Solution 2 : DP. 
+# Time Limit Exceeded. Time : O(N^2), Space : O(N)
+
+import math
+
+class Solution:    
+    def numSquares_2(self, n: int) -> int:
+        q, depth = {n}, 0
+        while q:
+            depth += 1
+            temp = set()
+            for i in q:
+                for j in range(1, int(n**0.5+1)):
+                    if i == j*j:
+                        return depth
+                    temp.add(i-j*j)
+                q = temp
+        return depth
+    
+    
+    def numSquares_1(self, n: int) -> int:
+        min_nums = [0]
+        for i in range(1, n+1):
+            min_num = float('inf')
+            for j in range(1, int(i**0.5+1)):
+                min_num = min(1 + min_nums[i-j**2], min_num)
+            min_nums.append(min_num)
+        return min_nums[n]
