3e521b343d6912f689ef1fbc371debde3ec9309a
hanwgyu <han.wgyu@gmail.com>
2019-09-04 23:51:31 +0900

    Update Perfect_Squares.cc

--- Leetcode/Perfect_Squares.cc
+++ Leetcode/Perfect_Squares.cc
@@ -4,7 +4,7 @@
 class Solution {
 public:
     int numSquares(int n) {
-        static std::vector<int> min_nums = {0};
+        static std::vector<int> min_nums {0};
         while(min_nums.size() <= n) {
             int min_num = INT_MAX, i = min_nums.size();
             for(int j = 1; j*j <= i; j++)
@@ -15,7 +15,7 @@ public:
     }
     
     int numSquares_1(int n) {
-        std::vector<int> min_nums = {0};
+        std::vector<int> min_nums {0};
         for(int i = 1; i < n+1; i++) {
             int min_num = INT_MAX;
             for(int j = 1; j*j <= i; j++)
