6e1fc24923e2b99eaadfdd3d60abd07ae6b561d4
hanwgyu <han.wgyu@gmail.com>
2019-09-16 19:52:07 +0900

    Update Climbing_Stairs.py

--- Leetcode/Climbing_Stairs.py
+++ Leetcode/Climbing_Stairs.py
@@ -1,11 +1,10 @@
-# Solution : DP. Time : O(N), Space : O(N)
+# Solution : DP. Time : O(N), Space : O(1)
 
 class Solution:
     def climbStairs(self, n: int) -> int:
         if n == 1:
             return 1
-        dp = [0 for _ in range(n)]
-        dp[0], dp[1] = 1, 2
+        step_2, step_1 = 1, 2
         for i in range(2, n):
-            dp[i] = dp[i-1] + dp[i-2]
-        return dp[n-1]
+            step_1, step_2 = step_2 + step_1, step_1
+        return step_1
