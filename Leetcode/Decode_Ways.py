d0dce10e9dc0a163bd993960b6a035d7f2b45dc9
hanwgyu <han.wgyu@gmail.com>
2019-09-28 16:20:12 +0900

    Update Decode_Ways.py

--- Leetcode/Decode_Ways.py
+++ Leetcode/Decode_Ways.py
@@ -1,6 +1,18 @@
 #Time : O(N), Space : O(1)
 
 class Solution:
+    # 진우님 솔루션, https://github.com/Curt-Park/algorithm_practice/blob/master/dp/decode_ways.py
+    def numDecodings(self, s: str) -> int:
+        """O(N) / O(1)"""
+        two, a, b, c = 0, 1, 1, 0
+        for ch in s:
+            one = int(ch)
+            two = two % 10 * 10 + one
+            c += b if 1 <= one <= 9 else 0
+            c += a if 10 <= two <= 26 else 0
+            a, b, c = b, c, 0
+        return b
+    
     def numDecodings(self, s: str) -> int:
         def canEncode(num : str) -> bool:
             if num[0] == '0': return False
