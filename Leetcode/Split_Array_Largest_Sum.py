3876509055d4edc15c086941cd7da7d50df9ef0c
hanwgyu <han.wgyu@gmail.com>
2019-10-05 22:52:52 +0900

    Update Split_Array_Largest_Sum.py

--- Leetcode/Split_Array_Largest_Sum.py
+++ Leetcode/Split_Array_Largest_Sum.py
@@ -2,7 +2,36 @@
 # Time : O(MN^2), Space : O(MN)
 # Leetcode 테스트 결과 시간초과
 
+# Solution 2 : Binary Search. Briiiiilliant!!!
+# Binary Search로 Valid(m개로 쪼갠 Subarray들의 합보다 큼) 한 가장 작은 값을 구하면 그값은 항상 Subarray의 합으로 표현할 수 있다!
+# Time: O(Nlog(sum(nums)-m)), Space : O(1)
+
 class Solution:
+    def splitArray_2(self, nums, m):
+        def valid(mid):
+            cnt = 0
+            current = 0
+            for n in nums:
+                current += n
+                if current>mid:
+                    cnt += 1
+                    if cnt>=m:
+                        return False
+                    current = n
+            return True
+
+        l = max(nums)
+        h = sum(nums)
+
+        while l<h:
+            mid = l+(h-l)//2
+            if valid(mid):
+                h = mid
+            else:
+                l = mid+1
+        return l
+    
+    
     def splitArray_1(self, nums: List[int], m: int) -> int:
         n = len(nums)
         dp = [[float('inf') for _ in range(min(i,m)+1)] for i in range(n+1)]
