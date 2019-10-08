44c21e3987309e038fedd14e786b056bb54e0145
hanwgyu <han.wgyu@gmail.com>
2019-10-09 01:19:36 +0900

    Create Reorder_Data_in_Log_Files.py

---
+++ Leetcode/Reorder_Data_in_Log_Files.py
@@ -0,0 +1,12 @@
+# Problem : let 으로 시작하는 원소를 dig 보다 앞에오게하고, let들은 뒷부분 문자열로 정렬하고, 문자가 똑같으면 id로 정렬.
+# Time : O(NlogN), Space : O(N)
+
+class Solution:
+    def sort(self, e):
+        i, log = e.split(' ', 1)
+        if log[0].isalpha():
+            return (0, log, i)
+        else:
+            return (1, None, None)
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        return sorted(logs, key = self.sort)
