3f891b8a0e3e8d6ade45dd66083e53256829070d
hanwgyu <han.wgyu@gmail.com>
2019-10-09 16:33:50 +0900

    Update Product_of_Array_Except_Self.py

--- Leetcode/Product_of_Array_Except_Self.py
+++ Leetcode/Product_of_Array_Except_Self.py
@@ -2,21 +2,14 @@
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        p, ans, zeros = 1, [], 0
+        ans = []
+        p = 1
         for num in nums:
-            if num != 0 : 
-                p *= num
-            else:
-                zeros += 1
-                
-        for num in nums:
-            if num != 0 and zeros > 0:
-                e = 0
-            elif num != 0:
-                e = p // num
-            elif zeros == 1:
-                e = p
-            else:
-                e = 0
-            ans.append(e)
+            ans.append(p)
+            p *= num
+        
+        p = 1
+        for i in reversed(range(len(nums))):
+            ans[i] *= p
+            p *= nums[i]
         return ans
