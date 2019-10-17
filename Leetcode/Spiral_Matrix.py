0d3d17d2777241d12804d1a40b82cc6eccc5b565
hanwgyu <han.wgyu@gmail.com>
2019-10-17 18:29:42 +0900

    Create Spiral_Matrix.py

---
+++ Leetcode/Spiral_Matrix.py
@@ -0,0 +1,16 @@
+#Time : O(MN), Space : O(1)
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:   
+        if not matrix:
+            return []
+        N, M, ans = len(matrix), len(matrix[0]), []
+        i, j, di, dj = 0, 0, 1, 0
+        for _ in range(M*N):
+            ans.append(matrix[j][i])
+            matrix[j][i] = 0
+            if (i == M-1 and j == 0) or (N != 1 and i == 0 and j == N-1) or (i == M-1 and j == N-1) or (matrix[j+dj][i+di] == 0):
+                di, dj = -dj, di
+            i += di
+            j += dj
+        return ans
