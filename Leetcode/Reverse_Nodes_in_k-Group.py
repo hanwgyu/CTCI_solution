a5fd6b4bc1f9b0a4325854f14a6a935d46ae7538
hanwgyu <han.wgyu@gmail.com>
2019-10-28 22:03:34 +0900

    Create Reverse_Nodes_in_k-Group.py

---
+++ Leetcode/Reverse_Nodes_in_k-Group.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+        def reverseK(before: ListNode, start: ListNode, k: int):            
+            if k < 2:
+                return (start, start)
+            front, back, target = start, start, start.next
+            for _ in range(k-1):
+                target.next, back.next = front, target.next
+                front, target = target, back.next
+            if before : before.next = front
+            return (front, back)
+        
+        if not head:
+            return None    
+        # 노드 전체 길이 계산
+        node, n = head, 0
+        while node:
+            node, n = node.next, n+1
+        
+        start, before = head, None
+        for i in range(n//k):
+            (front, before) = reverseK(before, start, k)
+            if i == 0: head = front
+            start = before.next
+        return head
