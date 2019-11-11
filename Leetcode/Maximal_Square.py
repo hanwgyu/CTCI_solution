e8e9f51588dea9b655dbd90f95ef9e2c260b9e4e
hanwgyu <han.wgyu@gmail.com>
2019-11-11 18:47:15 +0900

    Update Maximal_Square.py

--- Leetcode/Maximal_Square.py
+++ Leetcode/Maximal_Square.py
@@ -3,8 +3,34 @@
 # Solution 1 : DP.
 # Time : O(MN), Space : O(MN) 
 
+# Solution 1 : DP. 공간복잡도 줄임.
+# Time : O(MN), Space : O(min(M,N)) 
+
 class Solution:
-    def maximalSquare(self, matrix: List[List[str]]) -> int:
+    def maximalSquare_2(self, matrix: List[List[str]]) -> int:
+        if not matrix:
+            return 0
+        ans = 0
+        M, N = len(matrix), len(matrix[0])
+        N_large = True if M > N else False
+        S, L = M if N_large else N, N if N_large else M
+        current, past = [0] * S, [0] * S
+        
+        for a in range(L):
+            for b in range(S):
+                i, j = b if N_large else a, a if N_large else b
+                if matrix[i][j] == "1":
+                    res = 1
+                    if i > 0 and j > 0:
+                        res = min(past[b-1] + 1, current[b-1] + 1, past[b] + 1)
+                    current[b] = res
+                    ans = max(ans, res*res)
+            past, current = current, past
+            for b in range(S): current[b] = 0
+        return ans
+    
+    
+    def maximalSquare_1(self, matrix: List[List[str]]) -> int:
         if not matrix:
             return 0
         ans = 0
