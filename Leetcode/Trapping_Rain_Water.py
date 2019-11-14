77c2a30597838a6065d54f0bbdaec0ec8c3aef91
hanwgyu <han.wgyu@gmail.com>
2019-11-14 21:19:01 +0900

    Update Trapping_Rain_Water.py

--- Leetcode/Trapping_Rain_Water.py
+++ Leetcode/Trapping_Rain_Water.py
@@ -1,6 +1,6 @@
 #Time complexity : O(N), Space complexity : O(1)
 class Solution(object):
-    def trap(self, height):
+    def trap_1(self, height):
         # Solution : 가장 높은 Wall을 찾음. 양끝에서 시작해서, 가장 높은 wall에 다다를때까지 계산.
         # 지금까지 마주친 가장 높은 블록을 Wall로 설정하여, 새로운 블록을 마주칠때마다 차이값을 계산하여 더함.
         # 현재 Wall과 같은 높이거나 더 높은 블록을 마주칠때마다 Wall을 업데이트.
@@ -25,3 +25,16 @@ class Solution(object):
             total_water += (wall_height - height[i])
         
         return total_water
+    
+    def trap_2(self, height: List[int]) -> int:
+        l, r, l_max, r_max, ans = 0, len(height)-1, 0, 0, 0
+        while l < r:
+            if height[l] < height[r]:
+                l_max = max(l_max, height[l])
+                ans += l_max - height[l]
+                l += 1
+            else:
+                r_max = max(r_max, height[r])
+                ans += r_max - height[r]
+                r -= 1
+        return ans 
