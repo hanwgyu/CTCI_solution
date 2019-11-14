0d38e71a216b68fe6d1a59e3f83bebb9c85ca868
hanwgyu <han.wgyu@gmail.com>
2019-11-14 22:48:29 +0900

    Update Valid_Parentheses.py

--- Leetcode/Valid_Parentheses.py
+++ Leetcode/Valid_Parentheses.py
@@ -1,6 +1,18 @@
 #Time complexity : O(N), Space complexity : O(N)
 class Solution(object):
-    def isValid(self, s):
+    def isValid_2(self, s: str) -> bool:
+        st = []
+        d = {'{':'}', '[':']', '(':')'}
+        
+        for c in s:
+            if c in d.keys():
+                st.append(c)
+            elif c in d.values():
+                if not st or d[st.pop()] != c:
+                    return False
+        return not st
+    
+    def isValid_1(self, s):
         #Solution : Use stack 
         
         #Constants
