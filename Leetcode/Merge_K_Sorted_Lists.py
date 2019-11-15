1163c0070093c729718907b07f741c002e5cfe39
hanwgyu <han.wgyu@gmail.com>
2019-11-15 23:50:25 +0900

    Update Merge_K_Sorted_Lists.py

--- Leetcode/Merge_K_Sorted_Lists.py
+++ Leetcode/Merge_K_Sorted_Lists.py
@@ -1,6 +1,39 @@
-class Solution(object):
-    # Time Complexity : O(NlogK), Space Complexity : O(K)
-    def mergeKLists(self, lists):
+# Solution 1 : Heap 사용
+# Time Complexity : O(NlogK), Space Complexity : O(K)
+    
+# Solution 2 : list 두개끼리 merge. 
+# Time Complexity : O(Nlogk), Space Complexity : O(1)
+  
+class Solution:
+    def mergeKLists_2(self, lists: List[ListNode]) -> ListNode:
+        def mergeTwoList(first: ListNode, second: ListNode)-> ListNode:
+            dummy = ListNode(0)
+            cur = dummy
+            while first and second:
+                if first.val > second.val:
+                    cur.next, second = second, second.next
+                else:
+                    cur.next, first = first, first.next
+                cur = cur.next
+            if first:
+                cur.next = first    
+            elif second:
+                cur.next = second
+            return dummy.next
+        
+        temp = []
+        while lists or temp:
+            if len(lists) > 1:
+                temp.append(mergeTwoList(lists.pop(), lists.pop()))
+            else:
+                if not temp:
+                    return lists[0]
+                lists.extend(temp)
+                temp = []
+        return None
+    
+    
+    def mergeKLists_1(self, lists):
         K = len(lists)
         heads, heap = [head for head in lists], []
         for i in range(K):
