aa105c9b28523df69548f96f751f6655cbf57969
hanwgyu <han.wgyu@gmail.com>
2019-11-17 14:44:56 +0900

    Update Search_in_Rotated_Sorted_Array.py

--- Leetcode/Search_in_Rotated_Sorted_Array.py
+++ Leetcode/Search_in_Rotated_Sorted_Array.py
@@ -1,7 +1,40 @@
 # Time : O(logN), Space : O(1)
+# 범위 설정하는 것이 상당히 까다로움. 여러번 시행착오를 거쳐 해답을 찾아냈음.
+
+# Solution 3 : 일반 binary search하는 것처럼 동작시키기 위해 target과 nums[m]값이 
+# 위치하는 수열이 다른 경우, inf, -inf값으로 설정해 search를 동작시킨다.
 
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
+    def search_3(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums)-1        
+        while l <= r:
+            m = (l+r)//2
+            num = nums[m] if (target < nums[0]) == (nums[m] < nums[0]) else (float('-inf') if target < nums[0] else float('inf'))
+            if num < target:
+                l = m + 1
+            elif num > target:
+                r = m - 1
+            else:
+                return m
+        return -1
+
+    
+    def search_2(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums)-1
+        
+        while l <= r:
+            m = (l+r)//2
+            if nums[m] == target: return m
+            if nums[l] <= nums[m]:
+                if nums[l] <= target < nums[m]: r = m - 1
+                else: l = m + 1
+            else:
+                if nums[m] < target <= nums[r]: l = m + 1
+                else: r = m - 1
+        return -1
+                
+    
+    def search_1(self, nums: List[int], target: int) -> int:
         l, r = 0, len(nums) - 1
         while l < r:
             m = (l+r)//2
