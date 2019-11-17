e72c2daca56e5c8df839fdf98a83c1a19d81b0e2
hanwgyu <han.wgyu@gmail.com>
2019-11-17 15:13:49 +0900

    Update Subarray_Sum_Equals_K.py

--- Leetcode/Subarray_Sum_Equals_K.py
+++ Leetcode/Subarray_Sum_Equals_K.py
@@ -4,8 +4,19 @@
 # Diff of two elements are continuous subarrays ("sum(0...n) - sum(0..m) = sum(m...n) (if m<n)). 
 # Time : Avg O(N), Space : O(N)
 
+# Solution 2 : solution 1을 정리.
 
 class Solution(object):
+    def subarraySum_2(self, nums: List[int], k: int) -> int:
+        d = defaultdict(int)
+        d[0] = 1
+        total, ans = 0, 0
+        for n in nums:
+            total += n
+            ans += d[total-k]
+            d[total] += 1
+        return ans
+    
     def subarraySum_1(self, nums, k):
         a = {0:[-1]}
         ans, sum = 0, 0
