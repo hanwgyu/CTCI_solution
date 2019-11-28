02cfc8c56338521f5b19fad7cee246b408059987
hanwgyu <han.wgyu@gmail.com>
2019-11-28 23:56:43 +0900

    Update N-Queens.py

--- Leetcode/N-Queens.py
+++ Leetcode/N-Queens.py
@@ -1,9 +1,7 @@
 from collections import deque
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        def isPossible(cols: List[int], cur_row: int, cur_col: int, n: int) -> bool:
-            if cur_row < 0 or n <= cur_row:
-                return False
+        def isPossible(cols: List[int], cur_row: int, cur_col: int) -> bool:
             for row, col in enumerate(cols):
                 if cur_row == row or cur_col == col or cur_row-cur_col == row-col or cur_row+cur_col == row+col:
                     return False
@@ -13,7 +11,7 @@ class Solution:
                 return None
             ret = deque()
             for col in range(n):
-                if isPossible(cols, cur_row, col, n):
+                if isPossible(cols, cur_row, col):
                     if cur_row == n-1:
                         ret.append(deque(["."*col + "Q" + "."*(n-1-col)]))
                     tmp = findAllCases(cols+[col], cur_row+1, n)
