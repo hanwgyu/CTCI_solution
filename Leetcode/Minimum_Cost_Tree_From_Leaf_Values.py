3a03107938c103da44de404231aa28d3a24ef49e
hanwgyu <han.wgyu@gmail.com>
2019-12-01 22:24:21 +0900

    Update Minimum_Cost_Tree_From_Leaf_Values.py

--- Leetcode/Minimum_Cost_Tree_From_Leaf_Values.py
+++ Leetcode/Minimum_Cost_Tree_From_Leaf_Values.py
@@ -1,9 +1,27 @@
-# Burst Balloons 문제와 상당히 유사함.
-# Solution : DP + Merge. 반씩 잘라가면서 트리의 왼쪽 오른쪽으로 나누고 각 트리의 최소합과 최대 leaf node을 리턴
+# Solution 1 : DP + dfs. 반씩 잘라가면서 트리의 왼쪽 오른쪽으로 나누고 각 트리의 최소합과 최대 leaf node을 리턴
 # Time : O(N^2), Space : O(N^2) (DP없으면 O(N^3)?, O(N))
 
+# Solution 2 : Stack. Leetcode solution 참고함.
+# 'Trapping rain water' 문제와 동일하게 감소하는 stack을 유지해나아감. 값이 증가할때는 pop한 값과 min(pop이후 stack 마지막 원소, 추가하려는 원소) 를 곱해서 더함.
+# 이게 성립하는 이유는 곱들의 합의 최소값을 구하기 위해서는 곱들이 최대한 작은 값과 많이 계산이 되어야하기 때문. 
+# 큰 값은 최대한 나중에 곱하고, 작은 값끼리 곱함.
+# Time : O(N), Space : O(N)
+
 class Solution:
-    def mctFromLeafValues(self, arr: List[int]) -> int:
+    def mctFromLeafValues_2(self, arr: List[int]) -> int:
+        st, ans = [], 0
+        for i, v in enumerate(arr):
+            while st and arr[st[-1]] < v:
+                if len(st) > 1:
+                    ans += arr[st.pop()] * min(arr[st[-1]], v)
+                else:
+                    ans += arr[st.pop()] * v
+            st.append(i)
+        while len(st) > 1:
+            ans += arr[st.pop()] * arr[st[-1]]
+        return ans
+    
+    def mctFromLeafValues_1(self, arr: List[int]) -> int:
         def findMinSum(l: int, r: int) -> int:
             if r-l == 1:
                 return (0, arr[l])
