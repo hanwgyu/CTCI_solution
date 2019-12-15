434e0786b9efde5c3065f8820057ffa38363903a
hanwgyu <han.wgyu@gmail.com>
2019-12-15 17:19:41 +0900

    Create Keys_and_Rooms.py

---
+++ Leetcode/Keys_and_Rooms.py
@@ -0,0 +1,14 @@
+# Solution : DFS. Set에 룸을 추가해가면서, DFS를 돈 후 모든 룸이 추가되었으면 True. 
+# Time : O(|V|+|E|), Space : O(|V|)
+
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        def dfs(room: int):
+            if room in s:
+                return
+            s.add(room)
+            for key in rooms[room]:    
+                dfs(key)
+        s = set()
+        dfs(0)
+        return len(s) == len(rooms)
