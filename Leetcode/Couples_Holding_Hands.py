4dad5e3b19b053906ed3e063876d2606cc7804f7
hanwgyu <han.wgyu@gmail.com>
2019-12-27 21:34:26 +0900

    Update Couples_Holding_Hands.py

--- Leetcode/Couples_Holding_Hands.py
+++ Leetcode/Couples_Holding_Hands.py
@@ -2,7 +2,7 @@
 # 각 원소들의 idx를 통해 union-find 진행. 
 # Union 이후 남은 set의 갯수를 전체 원소 갯수에서 빼주면, 필요한 swap횟수가 나오게됨.
 # Time : O(N), Space: O(N)
-# Set의 swap 갯수 구하기 위한 개념 : Permutation Group. N-permutation group은 N-1개의 2-permutation group으로 표현 가능함.
+# Set의 swap 갯수 구하기 위한 개념 : Permutation Group. N-permutation group은 N-1개의 2-permutation group의 합성함수로 표현 가능함.
 
 
 from collections import defaultdict
