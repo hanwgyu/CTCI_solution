59ecab89ee97877e2b3d45c40d8e8e737109beab
Wangyu Han <han.wgyu@gmail.com>
2020-03-25 21:00:55 +0900

    Create Merge_Sorted_Array.py

---
+++ Leetcode/Merge_Sorted_Array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        while m > 0 and n > 0:
+            a, b = nums1[m-1], nums2[n-1]
+            nums1[m+n-1] = max(a, b)
+            if a > b: m = m-1
+            else: n = n-1
+        if m == 0:
+            for i in range(n):
+                nums1[i] = nums2[i]
