05012ee4b92c806bb776e434f2581e113691524d
Wangyu Han <han.wgyu@gmail.com>
2020-04-04 01:51:22 +0900

    Create Partition_to_K_Equal_Sum_Subsets.py

---
+++ Leetcode/Partition_to_K_Equal_Sum_Subsets.py
@@ -0,0 +1,21 @@
+# Solution : Backtracking + dfs.
+# Time : O(k * 2^N), Space : O(N)
+
+class Solution:
+    def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
+        def partition(nums: List[int], visited: List[bool], k: int, target: int, cur: int, index: int) -> bool:
+            if k == 0:
+                return True
+            if cur == target:
+                return partition(nums, visited, k-1, target, 0, 0)
+            for i in range(index, len(nums)):
+                if not visited[i] and target >= nums[i]+cur:
+                    visited[i] = True
+                    if partition(nums, visited, k, target, cur+nums[i], i+1):
+                        return True
+                    visited[i] = False
+            return False
+        visited = [False] * len(nums)
+        return partition(nums, visited, k, sum(nums) / k, 0, 0)
+        
+        
