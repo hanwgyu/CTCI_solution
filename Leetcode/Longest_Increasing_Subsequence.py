ead42bb4ca4a2a0947c9270794db2131ce73a94d
Wangyu Han <han.wgyu@gmail.com>
2020-04-28 23:25:12 +0900

    Update Longest_Increasing_Subsequence.py

--- Leetcode/Longest_Increasing_Subsequence.py
+++ Leetcode/Longest_Increasing_Subsequence.py
@@ -5,6 +5,7 @@
 # array내 숫자보다 큰 값이 올경우 원소를 추가하고, 그렇지 않을경우 적당한 위치에 업데이트함.
 # 이 방법을 이용해서 만든 array는 가능한 LIS리스트는 아님. LIS 리스트를 구할수 있는 방법이 있나 확인필요.
 # Time : O(NlogN), Space : O(N)
+# https://codedoc.tistory.com/414
 
 from bisect import bisect_left
 class Solution:
