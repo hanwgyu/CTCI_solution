56473792ac82d6bd433ecd6de195523b5a478e02
Wangyu Han <han.wgyu@gmail.com>
2020-11-10 23:02:47 +0900

    Update Count_Sorted_Vowel_Strings.py

--- Leetcode/Count_Sorted_Vowel_Strings.py
+++ Leetcode/Count_Sorted_Vowel_Strings.py
@@ -1,4 +1,4 @@
-# Solution 1. Time : O(1), Space : O(1)
+# Solution 1.
 """
 n개의 알파벳이 있을때, n-1 구간동안 최대 4번 증가가능.
 a1+a2+...+an-1 <= 4 (ai >= 0)
@@ -21,6 +21,15 @@ from math import comb
 
 
 class Solution:
-    def countVowelStrings(self, n: int) -> int:
+    def countVowelStrings_2(self, n: int) -> int:
+        """O(N) / O(1)"""
+        cnt = [1] * 5
+        for _ in range(1, n):
+            for i in range(5):
+                cnt[i] = sum(cnt[i:])
+        return sum(cnt)
+    
+    def countVowelStrings_1(self, n: int) -> int:
+        """O(1) / O(1)"""
         return comb(n + 4, n)
         # return 1 * comb(n+2, n-2) + 2 * comb(n+1, n-2) + 3 * comb(n, n-2) + 4 * comb(n-1,n-2) + 5 if n > 1 else 5
