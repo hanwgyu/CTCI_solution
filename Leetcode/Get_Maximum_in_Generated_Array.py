66abaf4f3cb8f8f2f4bd70bf7ad3195fc6776a80
Wangyu Han <han.wgyu@gmail.com>
2020-11-17 23:06:34 +0900

    Update Get_Maximum_in_Generated_Array.py

--- Leetcode/Get_Maximum_in_Generated_Array.py
+++ Leetcode/Get_Maximum_in_Generated_Array.py
@@ -3,18 +3,18 @@
 from collections import deque
 class Solution:
     def getMaximumGenerated(self, n: int) -> int:
-        if n < 2:
-            return n
+        if n == 0:
+            return 0
         q = deque([1])
-        ans, a = 0, 0
-        for i in range(2,n+1):
+        ans, a = 1, 0
+        for i in range(2, n + 1):
             if i % 2 == 0:
                 a = q.popleft()
                 q.append(a)
                 ans = max(ans, a)
             else:
-                q.append(a+q[0])
-                ans = max(ans, a+q[0])
+                q.append(a + q[0])
+                ans = max(ans, a + q[0])
         return ans
     
     
