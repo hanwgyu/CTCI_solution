ff20c56b257460899eea23e893fca2c37e7d57a3
Wangyu Han <han.wgyu@gmail.com>
2021-05-29 21:29:01 +0900

    Update Queue_Via_Stacks.py

--- Leetcode/Queue_Via_Stacks.py
+++ Leetcode/Queue_Via_Stacks.py
@@ -1,3 +1,57 @@
+"""
+self.st2가 비었을때만 self.st1에서 옮기도록 동작함.
+따라서 pop시에도 amortized O(1) 시간 복잡도가 가능함.
+"""
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.st1 = []
+        self.st2 = []
+        
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        self.st1.append(x)
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        self.peek()
+        return self.st2.pop()
+        
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        if not self.st2:
+            while self.st1:
+                self.st2.append(self.st1.pop())
+        return self.st2[-1]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        return not self.st1 and not self.st2
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
+
+----
+
 class MyQueue(object):
     def __init__(self):
         self.push_stack = []
