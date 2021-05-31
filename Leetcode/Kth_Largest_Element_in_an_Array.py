ff4a61c3b09d70f66068b949c2ff98b823ba5c16
Wangyu Han <han.wgyu@gmail.com>
2021-06-01 01:41:39 +0900

    Update Kth_Largest_Element_in_an_Array.py

--- Leetcode/Kth_Largest_Element_in_an_Array.py
+++ Leetcode/Kth_Largest_Element_in_an_Array.py
@@ -1,5 +1,40 @@
 import random
 
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        def quickSort(l, r):
+            if r-l <= 0:
+                return
+            pivot = random.randint(l, r)
+            pos = partition(l, r, pivot)
+            quickSort(l, pos-1)
+            quickSort(pos+1, r)
+        def partition(l, r, pivot):
+            nums[r], nums[pivot] = nums[pivot], nums[r]
+            pivot = r
+            r = r-1
+            while l <= r:
+                # desc order로 sorting.
+                if nums[l] >= nums[pivot]:
+                    l += 1
+                elif nums[pivot] >= nums[r]:
+                    r -= 1
+                else:
+                    nums[l], nums[r] = nums[r], nums[l]
+            print(nums, l, r, pivot)
+            nums[l], nums[pivot] = nums[pivot], nums[l]   
+            return l
+            
+        l, r = 0, len(nums) - 1
+        while True:
+            pivot = random.randint(l, r)
+            pos = partition(l, r, pivot)
+            if pos < k-1:
+                l = pos+1
+            elif pos > k-1:
+                r = pos-1
+            else:
+                return nums[k-1]
 
 class Solution(object):
     # Solution : Using quicksort
