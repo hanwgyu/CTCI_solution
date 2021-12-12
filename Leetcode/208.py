fee259f8b5289d9fda19f44036a2aff771e0bdfa
Wangyu Han <han.wgyu@gmail.com>
2021-12-13 00:15:50 +0900

    Solve

---
+++ Leetcode/208.py
@@ -0,0 +1,42 @@
+# REMIND: defaultdict으로 Trie 간단하게 생성하는 방법!
+
+class TrieNode:
+    def __init__(self):
+        self.children = defaultdict(TrieNode)
+        self.is_word = False
+
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for c in word:
+            # children에 TrieNode 가 없으면, 새로운 TrieNode를 생성함
+            node = node.children[c]
+        node.is_word = True
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                return False
+            node = node.children[c]
+        return node.is_word
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for c in prefix:
+            if c not in node.children:
+                return False
+            node = node.children[c]
+        return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
