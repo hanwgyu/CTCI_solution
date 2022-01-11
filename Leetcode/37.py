c51ff2cca4234ef3e877065aa31ef5f9e6b3ff92
Wangyu Han <han.wgyu@gmail.com>
2022-01-11 22:56:06 +0900

    Add

--- Leetcode/37.py
+++ Leetcode/37.py
@@ -6,6 +6,45 @@
 # REMIND: backtracking 기본문제.. 근데 못풀었다. 
 
 class Solution:
+    def solveSudoku(self, board):
+        """
+            dict을 사용해서 시간을 좀더 효율적으로,
+            원래 이방법으로 풀었는데, 깔끔하게 정리가 안되서 틀린듯.
+            이렇게 깔끔하게 푸는 방법을 배우는게 중요하다.
+        """
+        rows, cols, triples, visit = defaultdict(set), defaultdict(set), defaultdict(set), deque([])
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] != ".":
+                    rows[r].add(board[r][c])
+                    cols[c].add(board[r][c])
+                    triples[(r // 3, c // 3)].add(board[r][c])
+                else:
+                    visit.append((r, c))
+        
+        def dfs():
+            if not visit:
+                return True
+            r,c = visit[0]
+            t = (r//3, c//3)
+            for e in {"1", "2", "3", "4", "5", "6", "7", "8", "9"}: 
+                if e not in rows[r] and e not in cols[c] and e not in triples[t]:
+                    board[r][c] = e
+                    rows[r].add(e)
+                    cols[c].add(e)
+                    triples[t].add(e)
+                    visit.popleft()
+                    if dfs():
+                        return True
+                    board[r][c] = "."
+                    rows[r].remove(e)
+                    cols[c].remove(e)
+                    triples[t].remove(e)
+                    visit.appendleft((r, c))
+            return False
+        dfs()
+
+
     def solveSudoku(self, board: List[List[str]]) -> None: # in-place
         """
             정말로 그냥 다해보는 방법..
