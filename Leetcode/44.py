b36d008deac2f6757c4e237c34a529e26c0aeef6
Wangyu Han <han.wgyu@gmail.com>
2022-01-17 22:16:43 +0900

    Solve

--- Leetcode/44.py
+++ Leetcode/44.py
@@ -6,6 +6,31 @@
 
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
+        """
+        DP로 푸는게 훨씬 쉽긴하다.
+        """
+        dp = defaultdict(bool)
+        M, N = len(s), len(p)
+        dp[(0,0)] = True
+        # 예외 처리
+        for j in range(1, N+1):
+            if p[j-1] != '*':
+                break
+            dp[(0, j)] = True
+        
+        for j, n in enumerate(p, start=1):
+            for i, m in enumerate(s, start=1):
+                if n == '*':
+                    dp[(i,j)] = dp[(i-1, j-1)] or dp[(i-1,j)] or dp[(i,j-1)]
+                elif n in ('?', m):
+                    dp[(i,j)] = dp[(i-1, j-1)]
+        return dp[(M, N)]
+        
+         
+
+            
+    
+    def isMatch1(self, s: str, p: str) -> bool:
         M, N = len(s), len(p)
         @lru_cache(None)
         def dfs(i: int, j: int) -> bool:
