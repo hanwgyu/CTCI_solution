e239380313b7373e595b860cc2846165a9a4e09a
Wangyu Han <han.wgyu@gmail.com>
2022-03-28 23:09:19 +0900

    Update Combination_Sum.py

--- Leetcode/Combination_Sum.py
+++ Leetcode/Combination_Sum.py
@@ -1,5 +1,23 @@
 from collections import defaultdict
 
+from functools import lru_cache
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        @lru_cache(maxsize=None)
+        def dfs(i, target: int) -> List[List[int]]:
+            if target == 0:
+                return [[]]
+            ans = []
+            for j in range(i,len(candidates)):
+                c = candidates[j]
+                if target-c >= 0:
+                    for l in dfs(j, target-c):
+                        ans.append([c]+l)
+            return ans
+        return dfs(0, target)
+
 
 class Solution:
     def combinationSum(
