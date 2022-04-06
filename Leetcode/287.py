73fee1ac6788375a69eb209c2bf5bb307a112365
Wangyu Han <han.wgyu@gmail.com>
2022-04-07 00:39:37 +0900

    Solve

--- Leetcode/287.py
+++ Leetcode/287.py
@@ -1,3 +1,7 @@
+""" 
+세가지 다 천재적인 방법.
+"""
+
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         """
@@ -34,3 +38,24 @@ class Solution:
                 ans = m
                 l = m+1
         return ans
+
+    def findDuplicate(self, nums: List[int]) -> int:
+        """
+        특정 숫자와 그 숫자에 해당하는 인덱스의 숫자를 linked list로 연결한다고 생각하면,
+        겹치는 부분에서 cycle이 시작되는 형태가 된다.
+        cycle의 head 는 항상 nums[0]. 0을 가리키는 곳은 없다.
+        
+        cycle 시작하는 부분을 구하는건 https://leetcode.com/problems/linked-list-cycle-ii/ 이 문제와 동일하게 푼다.
+
+        O(N) / O(1) 와우... 천재인가?
+        """
+        fast = slow = nums[0]
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                slow = nums[0]
+                while fast != slow:
+                    slow = nums[slow]
+                    fast = nums[fast]
+                return fast
