149628be1e1977ce69b44f90f421ccfe1330b906
Wangyu Han <han.wgyu@gmail.com>
2022-04-16 23:54:55 +0900

    Update 162.py

--- Leetcode/162.py
+++ Leetcode/162.py
@@ -13,3 +13,15 @@ class Solution:
             else:
                 l = m + 1
         return l
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        N = len(nums)
+        l, r, m = 0, N-1, 0
+        while l < r:
+            m = (l+r)//2
+            if nums[m] > nums[m+1]:
+                r = m
+            else:
+                l = m + 1
+        return l
