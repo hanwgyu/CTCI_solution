9b26739bc0c4ff76b252c4ed9cfa05ecfb3b2886
Wangyu Han <han.wgyu@gmail.com>
2022-04-21 01:15:27 +0900

    Update Word_Search.py

--- Leetcode/Word_Search.py
+++ Leetcode/Word_Search.py
@@ -2,32 +2,39 @@
 
 from collections import defaultdict
 
-
 class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
-        def dfs(x: int, y: int, index: int, visited) -> bool:
-            if visited[(x, y)]:
+    def exist(self, matrix: List[List[str]], word: str) -> bool:
+        M, N = len(matrix), len(matrix[0])
+        def dfs(i: int, j: int, k: int, visited: Set[Tuple[int, int]]) -> bool:
+            if k == len(word): return True
+            if i < 0 or i >= M or j < 0 or j >= N or (i,j) in visited or matrix[i][j] != word[k]:
                 return False
-            visited[(x, y)] = True
+            visited.add((i,j))
+            res = dfs(i, j+1, k+1, visited) or dfs(i, j-1, k+1, visited) or dfs(i+1, j, k+1, visited) or dfs(i-1, j, k+1, visited)
+            visited.remove((i,j))
+            return res
+        return any(dfs(i, j, 0, set()) for i in range(M) for j in range(N))
+
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        M, N = len(board), len(board[0])
+        def dfs(x: int, y: int, index: int, visited: Set[int]) -> bool:
+            if (x,y) in visited:
+                return False
+            visited.add((x,y))
             if board[x][y] == word[index]:
                 if index == len(word) - 1:
                     return True
                 for (dx, dy) in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-                    if (
-                        x + dx < 0
-                        or x + dx >= len(board)
-                        or y + dy < 0
-                        or y + dy >= len(board[0])
-                    ):
-                        continue
-                    if dfs(x + dx, y + dy, index + 1, visited):
-                        return True
-            visited[(x, y)] = False
+                    x_, y_ = x+dx, y+dy
+                    if 0 <= x_ < M and 0<= y_ < N:
+                        if dfs(x + dx, y + dy, index + 1, visited):
+                            return True
+            visited.remove((x,y))
             return False
 
         for i in range(len(board)):
             for j in range(len(board[0])):
-                visited = defaultdict(bool)
-                if dfs(i, j, 0, visited):
+                if dfs(i, j, 0, set()):
                     return True
         return False
+
