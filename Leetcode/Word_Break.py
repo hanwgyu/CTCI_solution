2f5bd62957f2c86f0961243581dc4f09506b592e
Wangyu Han <han.wgyu@gmail.com>
2022-06-22 21:36:26 +0900

    Update Word_Break.py

--- Leetcode/Word_Break.py
+++ Leetcode/Word_Break.py
@@ -53,3 +53,10 @@ class Solution:
                     dp[i] = True
                     break
         return dp[0]
+
+    
+    def wordBreak(self, s, words):
+        ok = [True]
+        for i in range(1, len(s)+1):
+            ok += any(ok[j] and s[j:i] in words for j in range(i)),
+        return ok[-1]
